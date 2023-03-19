0e3dd01057963440d6b89441f3fb69b4190f075c
orion-japan <arigatousun8@gmail.com>
2023-03-19 17:02:14 +0900

    first commit

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
-  "";
+  "0x3B4eb956960F928f6363659d28795BC6b01280c0";
 export const tokenContractAddress =
-  "";
+  "0x040cAC119c14126528480464F3396538143AC878";
 export const stakingContractAddress =
-  "";
+  "0xa115BF36099b6DCefae9D3c38C95C837F2E38E68";
